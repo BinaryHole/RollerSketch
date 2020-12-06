3adc2e573fcd4d3269d10d62606e2dc96c7d92ce
Mark McGregor <mark.mcgregor.contact@gmail.com>
2020-12-05 20:54:40 -0500

    Update motherboard Sketch to use buttons and distance sensor

--- Arduino Sketches/Roll__n_Sketch_Motherboard/Roll__n_Sketch_Motherboard.ino
+++ Arduino Sketches/Roll__n_Sketch_Motherboard/Roll__n_Sketch_Motherboard.ino
@@ -1,14 +1,25 @@
 #include <SPI.h>
 #include <NRFLite.h>
+#include <HCSR04.h>
 
 // define NRF transciever unit
 NRFLite _radio;
+
+// define US sensor unit
+const int TRIG = 7;
+const int ECHO = 6;
+UltraSonicDistanceSensor usSensor(TRIG, ECHO);
 
 // define radio constants
 const int RADIO_NUM = 0;
 const int TRANS_NUM = 1;
 const int CE        = 9;
 const int CSN       = 10;
+
+// define button inputs
+const int BUTTON_1 = 2;
+const int BUTTON_2 = 3;
+const int BUTTON_3 = 4;
 
 // define a radio packet's data
 struct RadioPacket {
@@ -19,6 +30,14 @@ struct RadioPacket {
 // declare global RadioData variable
 RadioPacket _radioData;
 
+// declare global button values
+int button1Val = 0;
+int button2Val = 0;
+int button3Val = 0;
+
+// define global ultrasonic value
+int usDistanceVal = 0;
+
 void setup() {
   // open the serial port
   Serial.begin(115200);
@@ -26,14 +45,41 @@ void setup() {
 
   // setup the radio
   _radio.init(RADIO_NUM, CE, CSN);
+
+  // setup the buttons
+  pinMode(BUTTON_1, INPUT_PULLUP);
+  pinMode(BUTTON_2, INPUT_PULLUP);
+  pinMode(BUTTON_3, INPUT_PULLUP);
 }
 
 void loop() {
   // read the incoming radio data
   readRadio();
 
+  // read the button values
+  readButtons();
+
+  // read the ultrasonic distance value
+  readUsSensor();
+
   // print the radio data to the serial port
   sendDataToSerial();
+  //forceDataToSerial();
+}
+
+void readButtons() {
+  button1Val = digitalRead(BUTTON_1);
+  button2Val = digitalRead(BUTTON_2);
+  button3Val = digitalRead(BUTTON_3);
+}
+
+void readUsSensor() {
+  double newDistanceVal = usSensor.measureDistanceCm();
+
+  if (0 <= newDistanceVal && newDistanceVal <= 50) {
+    //usDistanceVal = newDistanceVal;
+    usDistanceVal = floor(newDistanceVal);
+  }
 }
 
 void readRadio() {
@@ -42,10 +88,36 @@ void readRadio() {
   }
 }
 
+void forceDataToSerial() {
+  Serial.print(_radioData.pitch);
+  Serial.print(",");
+  Serial.print(_radioData.roll);
+  Serial.print(",");
+  Serial.print(button1Val);
+  Serial.print(",");
+  Serial.print(button2Val);
+  Serial.print(",");
+  Serial.print(button3Val);
+  Serial.print(",");
+  Serial.print(usDistanceVal);
+  
+  Serial.println();
+}
+
 void sendDataToSerial() {
   if (Serial.available() > 0 && Serial.read() == 'r') {
     Serial.print(_radioData.pitch);
     Serial.print(",");
-    Serial.println(_radioData.roll);
+    Serial.print(_radioData.roll);
+    Serial.print(",");
+    Serial.print(button1Val);
+    Serial.print(",");
+    Serial.print(button2Val);
+    Serial.print(",");
+    Serial.print(button3Val);
+    Serial.print(",");
+    Serial.print(usDistanceVal);
+    
+    Serial.println();
   }
 }
